2388727d6f953d2e9c154bef8768e990358dacbd
Sithija Sumanga <iidsithija@gmail.com>
2024-02-04 16:53:38 +0530

    test 3

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,7 @@
-import express, { Request, Response } from "express";
 import next from "next";
 import path from "path";
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev, dir: path.join(__dirname, "views") });
-const handle = app.getRequestHandler();
-
-app.prepare().then(() => {
-  const server = express();
-
-  // Your Express routes go here
-  // server.get("/custom-route", (req: Request, res: Response) => {
-  //   const query = req.query as ParsedUrlQuery;
-  //   return app.render(req, res, "/custom-page", query);
-  // });
-
-  // Default route handling by Next.js
-  server.all("*", (req, res) => {
-    return handle(req, res);
-  });
-
-  const PORT = process.env.PORT || 3000;
-
-  server.listen(PORT, (err?: Error) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${PORT}`);
-  });
-});
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
-import app from './app';
+import app from "./app";
+import express, { Request, Response } from "express";
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
-  /* eslint-enable no-console */
+const handle = app.getRequestHandler();
+
+// const port = process.env.PORT || 5000;
+app.prepare().then(() => {
+  const server = express();
+
+  // Your Express routes go here
+  // server.get("/custom-route", (req: Request, res: Response) => {
+  //   const query = req.query as ParsedUrlQuery;
+  //   return app.render(req, res, "/custom-page", query);
+  // });
+
+  // Default route handling by Next.js
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  const PORT = process.env.PORT || 3000;
+
+  server.listen(PORT, (err?: Error) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${PORT}`);
+  });
 });
