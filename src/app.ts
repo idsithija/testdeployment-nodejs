b65b67e068bdf2ab2daf1d89faee56de87b91ce4
Sithija Sumanga <iidsithija@gmail.com>
2024-02-04 16:50:32 +0530

    test 2

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from "express";
 import next from "next";
-import { ParsedUrlQuery } from "querystring";
 import path from "path";
 
 const dev = process.env.NODE_ENV !== "production";
@@ -11,10 +10,10 @@ app.prepare().then(() => {
   const server = express();
 
   // Your Express routes go here
-  server.get("/custom-route", (req: Request, res: Response) => {
-    const query = req.query as ParsedUrlQuery;
-    return app.render(req, res, "/custom-page", query);
-  });
+  // server.get("/custom-route", (req: Request, res: Response) => {
+  //   const query = req.query as ParsedUrlQuery;
+  //   return app.render(req, res, "/custom-page", query);
+  // });
 
   // Default route handling by Next.js
   server.all("*", (req, res) => {
